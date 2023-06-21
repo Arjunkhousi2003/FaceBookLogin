6ddbbb2a1e4a45a6a410efe3fbe0a75c14df219e
nidhiraj <nidhi17may@gmail.com>
2023-06-21 13:49:01 +0530

    raj update1

--- app/src/main/java/com/example/facebooklogin/Adapter_RecyclerView.java
+++ app/src/main/java/com/example/facebooklogin/Adapter_RecyclerView.java
@@ -10,5 +10,6 @@ public class Adapter_RecyclerView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adapter_recycler_view);
+        //aaaaaaaaaa
     }
 }
