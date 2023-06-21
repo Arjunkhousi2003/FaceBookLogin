ec641aa0f85116ffb917b2d73ab3ad0bfb1360b7
nidhiraj <nidhi17may@gmail.com>
2023-06-21 13:24:37 +0530

    newtext

--- app/src/main/java/com/example/facebooklogin/Adapter_RecyclerView.java
+++ app/src/main/java/com/example/facebooklogin/Adapter_RecyclerView.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Adapter_RecyclerView extends AppCompatActivity {
-
+//jsdgyusagdyfasfdtyasfdftasfd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
